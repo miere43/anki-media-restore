4dbbeaafae77c906bd0759c104ab14abb9f84ae6
miere43 <x.miere@gmail.com>
2020-11-25 20:26:20 +0300

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sourcePath = args[0];
+            var destinationPath = args[1];
+
+            using var mediaStream = File.OpenRead(Path.Join(sourcePath, "media"));
+            var doc = JsonDocument.Parse(mediaStream);
+
+            foreach (var item in doc.RootElement.EnumerateObject())
+            {
+                var filePath = Path.Join(sourcePath, item.Name);
+                var newFilePath = Path.Join(destinationPath, item.Value.GetString());
+                File.Copy(filePath, newFilePath);
+                Console.WriteLine($"{filePath} => {newFilePath}");
+            }
+
+            Console.WriteLine("Done");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+## Anki Media Renamer
+
+Restores original file names from *.apkg file.
+
+Unpack *.apkg file contents and specify path to unpacked data as first argument. Second argument is destination folder where files with restored file names will be placed.
+
+Usage: program.exe "source_path" "destination_path"
